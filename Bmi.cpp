4b18909e0b41783528d02def99b314e2b118d070
Younes Nosrati <77382993+younii770@users.noreply.github.com>
2021-10-08 16:49:13 +0330

    Update Bmi.cpp

--- Bmi.cpp
+++ Bmi.cpp
@@ -2,11 +2,7 @@
 using namespace std;
 int main()
 {
-
-
-
 	float a, b, c;
-
 	cout << " Enter your weight in Kg:";
 	cin >>  a;
 	cout << " Enter your height in Meters: ";
@@ -16,17 +12,14 @@ int main()
 	if (c < 18.5)
 	{
 		cout << " Underweight:";
-
 	}
 	else if (18.5 < c && c < 24.9)
 	{
 		cout << " Normal";
-
 	}
 	else if (25 < c && c < 29.9)
 	{
 		cout << " Overweight";
-
 	}
 	else if (30 < c && c < 34.9)
 	{
@@ -34,7 +27,6 @@ int main()
 	}
 	else if (35 < c)
 	{
-
 		cout << " Extremly obese";
 	}
 
